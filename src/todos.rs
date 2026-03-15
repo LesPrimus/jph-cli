c7e6c8f94cbfeb7f5674cbeef953a9660e6eb5a2
LesPrimus <amerigo.armentano@hotmail.it>
2026-03-15 07:32:04 +0100

    Extract `ToDo` logic into a separate `todos` module.

---
+++ src/todos.rs
@@ -0,0 +1,30 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, thiserror::Error)]
+pub enum ToDoError {
+    #[error(transparent)]
+    Network(#[from] reqwest::Error),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ToDo {
+    completed: bool,
+    id: i32,
+    title: String,
+    user_id: i32,
+}
+
+impl ToDo {
+    const TARGET: &str = "https://jsonplaceholder.typicode.com/todos";
+
+    pub async fn get_all(client: &Client) -> Result<Vec<ToDo>, ToDoError> {
+        Ok(client.get(Self::TARGET).send().await?.json().await?)
+    }
+
+    pub async fn get_by_id(client: &Client, id: i32) -> Result<ToDo, ToDoError> {
+        let target = format!("{}/{}", Self::TARGET, id);
+        Ok(client.get(target).send().await?.json().await?)
+    }
+}
